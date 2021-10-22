a05db2b2e9b8ad88327699ae3e5420ac06c1bcc7
remov_b4_flight <jenoki@mac.com>
2021-10-23 07:07:40 +0900

    0.51
    version feeding (avoid LrE-6 conflict)

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -33,7 +33,7 @@ extern "C" {
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #define USBD_DEVICE_VER_MAJ	0x00
-#define USBD_DEVICE_VER_MIN	0x49
+#define USBD_DEVICE_VER_MIN	0x51
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
